b38525e24b3d88aefe3b4182ef6a1bd8b8eafb38
Poshan <poshan.niraula@gmail.com>
2014-05-18 11:52:42 +0545

    stylechooser

--- js/style_chooser.js
+++ js/style_chooser.js
@@ -1,12 +1,16 @@
-styleChooserDiv = $('#styleChooser');
-
+//styleChooserDiv = $('#styleChooser');
+var styleChooserDiv = document.getElementById("styleChooser");
 htcStyles = ["supported", "district"];
-
+var rdiv = document.createElement('div');
+rdiv.setAttribute("class", "btn-group-vertical");
+rdiv.setAttribute("data-toggle", "modal");
+styleChooserDiv.appendChild(rdiv);
 $.each(htcStyles, function(index, val) {
     button = document.createElement('input');
+    button.setAttribute("class", "btn btn-primary");
     button.value = val;
     button.innerHtml = val;
-    button.type = 'radio';
+    button.type = 'button';
     button.name = "htcStyles";
-    styleChooserDiv.append(button);
+    rdiv.appendChild(button);
 });
