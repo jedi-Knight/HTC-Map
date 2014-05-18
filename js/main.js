fd538de6c256d9ef284bbc0411b4a53932635a56
Poshan <poshan.niraula@gmail.com>
2014-05-18 11:04:39 +0545

    legend

--- js/main.js
+++ js/main.js
@@ -13,12 +13,45 @@
                 nep_latlng_array.push(b);
             }
         }
+
+        function getColor(d) {
+            return d > 1000 ? '#800026' :
+                d > 500 ? '#BD0026' :
+                d > 200 ? '#E31A1C' :
+                d > 100 ? '#FC4E2A' :
+                d > 50 ? '#FD8D3C' :
+                d > 20 ? '#FEB24C' :
+                d > 10 ? '#FED976' :
+                '#FFEDA0';
+        }
+
         var map = L.map('map').setView([28.425, 84.435], 7),
             osmUrl = 'https://a.tiles.mapbox.com/v3/poshan.i65ff4hn/{z}/{x}/{y}.png',
             osmAttribution = 'Map data &copy; 2012 OpenStreetMap contributors';
         var osm = L.TileLayer.boundaryCanvas(osmUrl, {
             boundary: nep_latlng_array
         }).addTo(map);
+
+        var legend = L.control({
+            position: 'bottomleft'
+        });
+        legend.onAdd = function(map) {
+
+            var div = L.DomUtil.create('div', 'info legend'),
+                grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+                labels = [];
+
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < grades.length; i++) {
+                div.innerHTML +=
+                    '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            }
+
+            return div;
+        };
+
+        legend.addTo(map);
 
         function highlightFeature(e) {
             var layer = e.target;
@@ -129,6 +162,7 @@
                     district_boundary.addData(data);
 
                 });
+
 
             }
         });
