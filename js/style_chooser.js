552a94bdaa693933233a7831c3f62a62cef030db
jediKnight <gautamxpratik@gmail.com>
2014-05-22 12:06:56 +0545

    Update style_chooser.js
    commented out code for those two blue buttons..left for further cleanup..

--- js/style_chooser.js
+++ js/style_chooser.js
@@ -1,5 +1,30 @@
-//styleChooserDiv = $('#styleChooser');
+/////////////////////styleChooserDiv = $('#styleChooser');
 var styleChooserDiv = document.getElementById("styleChooser");
+htcStyles = ["supported", "cases"];
+var rdiv = document.createElement('div');
+/***********/
+//rdiv.setAttribute("class", "btn-group-vertical");
+//rdiv.setAttribute("data-toggle", "modal");
+//styleChooserDiv.appendChild(rdiv);
+/**********/
+
+function clickfunction(id) {
+    alert(id);
+    // HTC_sites.redraw();
+    HTC_sites.setStyle();
+}
+
+$.each(htcStyles, function(index, val) {
+    button = document.createElement('input');
+    button.setAttribute("class", "btn btn-primary");
+    button.value = val;
+    button.innerHtml = val;
+    button.id = index;
+    button.type = 'button';
+    button.name = "htcStyles";
+    button.setAttribute("onclick", "clickfunction(this.value)");
+    rdiv.appendChild(button);
+});
 
 $.each(styles, function(index, val1) {
     switch (val1['geometry']) {
@@ -9,7 +34,7 @@ $.each(styles, function(index, val1) {
                 text: val1['display']
             }).appendTo(styleChooserDiv);
             $.each(val1['styles'], function(index, val2) {
-                styleDiv = $('<button>', {
+                styleDiv = $("<button>", {
                     text: index,
                     click: function() {
                         val1['layer'].eachLayer(val1['styles'][index]);
@@ -27,7 +52,7 @@ $.each(styles, function(index, val1) {
                     text: index,
                     click: function() {
                         console.log("val1=", val1);
-                        val1['layer'].setStyle(val1['styles'][index]);
+                        val1['layer'].setStyle(val1['styles'][index])
                     }
                 }).appendTo(nameDiv);
             });
