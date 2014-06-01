fcb77904f63dd7b6e961ea11c4051a788565a6be
amritkarma <amrit.im@gmail.com>
2014-06-01 14:18:42 +0545

    corrected radio buttons functions in style_chooser

--- js/styles.js
+++ js/styles.js
@@ -26,8 +26,8 @@ function style_htc(marker) {
 function style_htc_default(marker) {
     var HTC_icon = L.icon({
         iconUrl: 'img/htc.png',
-        iconSize: [38, 95], // size of the icon
-        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        iconSize: [28, 25], // size of the icon
+        // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
         popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
     return marker.setIcon(HTC_icon)
