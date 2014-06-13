8cefc749844b88a57e97ba236df158e3e4f809c2
Poshan <poshan.niraula@gmail.com>
2014-06-13 14:01:17 +0545

    working on district popup

--- js/layers.js
+++ js/layers.js
@@ -79,45 +79,45 @@ function districtpopUp(feature, layer) {
     popUp += layer.feature.properties.district_Contact;
     popUp += '</br>';
     popUp += layer.feature.properties.district_Cum_death_2010;
-    /*district_Cum_death_2011;
-    district_Cum_death_2012;
-    district_Cum_on_ART_2010;
-    district_Cum_on_ART_2011;
-    district_Cum_on_ART_2012;
-    district_Est_FSWs;
-    district_Est_MTC;
-    district_HIV_Pos_Female_2010;
-    district_HIV_Pos_Female_2011;
-    district_HIV_Pos_Female_2012;
-    district_HIV_Pos_TG_2012;
-    district_HIV_Pos_delivery_2010;
-    district_HIV_Pos_delivery_2011;
-    district_HIV_Pos_delivery_2012;
-    district_HIV_Pos_male_2010;
-    district_HIV_Pos_male_2011;
-    district_HIV_Pos_male_2012;
-    district_IA_TI_FSW: ""
-    district_IA_TI_MSM: ""
-    district_IA_TI_Migrants: ""
-    district_IA_TI_PLHIV: ""
-    district_IA_TI_PWIDs: ""
-    district_IA_TI_Prison: ""
-    district_No_of_ART_OI: "1"
-    district_No_of_CCC: "2"
-    district_No_of_CCC_FHI360: ""
-    district_No_of_CCC_FPAN: ""
-    district_No_of_CCC_GoV: ""
-    district_No_of_CCC_Others: ""
-    district_No_of_CCC_PF: ""
-    district_No_of_CCC_Save: ""
-    district_No_of_CHBC: ""
-    district_No_of_CHBC_FHI360: ""
-    district_No_of_CHBC_FPAN: ""
-    district_No_of_CHBC_GoV: ""
-    district_No_of_CHBC_Save: ""
-    district_No_of_CHBC_Save_1: ""
-    district_No_of_CHBC_others: ""
-    district_No_of_HIV_tested_2010: "2463"
+    /*popUp += layer.feature.properties.district_Cum_death_2011;
+    popUp += layer.feature.properties.district_Cum_death_2012;
+    popUp += layer.feature.properties.district_Cum_on_ART_2010;
+    popUp += layer.feature.properties.district_Cum_on_ART_2011;
+    popUp += layer.feature.properties.district_Cum_on_ART_2012;
+    popUp += layer.feature.properties.district_Est_FSWs;
+    popUp += layer.feature.properties.district_Est_MTC;
+    popUp += layer.feature.properties.district_HIV_Pos_Female_2010;
+    popUp += layer.feature.properties.district_HIV_Pos_Female_2011;
+    popUp += layer.feature.properties.district_HIV_Pos_Female_2012;
+    popUp += layer.feature.properties.district_HIV_Pos_TG_2012;
+    popUp += layer.feature.properties.district_HIV_Pos_delivery_2010;
+    popUp += layer.feature.properties.district_HIV_Pos_delivery_2011;
+    popUp += layer.feature.properties.district_HIV_Pos_delivery_2012;
+    popUp += layer.feature.properties.district_HIV_Pos_male_2010;
+    popUp += layer.feature.properties.district_HIV_Pos_male_2011;
+    popUp += layer.feature.properties.district_HIV_Pos_male_2012;
+    popUp += layer.feature.properties.district_IA_TI_FSW;
+    popUp += layer.feature.properties.district_IA_TI_MSM;
+    popUp += layer.feature.properties.district_IA_TI_Migrants: ""
+    popUp += layer.feature.properties.district_IA_TI_PLHIV: ""
+    popUp += layer.feature.properties.district_IA_TI_PWIDs: ""
+    popUp += layer.feature.properties.district_IA_TI_Prison: ""
+    popUp += layer.feature.properties.district_No_of_ART_OI: "1"
+    popUp += layer.feature.properties.district_No_of_CCC: "2"
+    popUp += layer.feature.properties.district_No_of_CCC_FHI360: ""
+    popUp += layer.feature.properties.district_No_of_CCC_FPAN: ""
+    popUp += layer.feature.properties.district_No_of_CCC_GoV: ""
+    popUp += layer.feature.properties.district_No_of_CCC_Others: ""
+    popUp += layer.feature.properties.district_No_of_CCC_PF: ""
+    popUp += layer.feature.properties.district_No_of_CCC_Save: ""
+    popUp += layer.feature.properties.district_No_of_CHBC: ""
+    popUp += layer.feature.properties.district_No_of_CHBC_FHI360: ""
+    popUp += layer.feature.properties.district_No_of_CHBC_FPAN: ""
+    popUp += layer.feature.properties.district_No_of_CHBC_GoV: ""
+    popUp += layer.feature.properties.district_No_of_CHBC_Save: ""
+    popUp += layer.feature.properties.district_No_of_CHBC_Save_1: ""
+    popUp += layer.feature.properties.district_No_of_CHBC_others: ""
+    popUp += layer.feature.properties.district_No_of_HIV_tested_2010: "2463"
     district_No_of_HIV_tested_2011: "2358"
     district_No_of_HIV_tested_2012: "3108"
     district_No_of_HTC: "3"
