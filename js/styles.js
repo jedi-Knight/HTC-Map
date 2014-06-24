6744aefc28dfe053eab534b70e48ce9d4ee46bf7
jediKnight <gautamxpratik@gmail.com>
2014-06-24 14:07:36 +0545

    Merge originhtc/master
    Conflicts:
    	css/gui.css
    	index.html
    
    changed iconsize in legend;
    scroll legend when larger than 325px;

--- js/styles.js
+++ js/styles.js
@@ -115,7 +115,7 @@ function iconToLegendString() {
     var legendHTML = "";
     for (icon in icons) {
         //
-        legendHTML += "<div><img src ='" + icons[icon].options.iconUrl + "' style = 'height:40'>" + icon + "</div>";
+        legendHTML += "<div><img src ='" + icons[icon].options.iconUrl + "' style = 'height:30'>" + icon + "</div>";
     }
     return legendHTML;
 }
