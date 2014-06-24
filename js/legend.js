3f9c1c8a1acd9d1ce07d37a164eb16b9d50bf417
amritkarma <amrit.im@gmail.com>
2014-06-24 10:23:08 +0545

    banner from pratik

--- js/legend.js
+++ js/legend.js
@@ -26,7 +26,7 @@ legend.update = function(legends) {
     }
     // write out html for legend
     for (le in this._legends) {
-        todisplay += "<div><h6>" + le + "</h6></div><div>" + this._legends[le] + "</div>";
+        todisplay += "<div><h5>" + le + "</h5></div><div>" + this._legends[le] + "</div>";
         todisplay += "";
     }
     this._div.innerHTML = todisplay
